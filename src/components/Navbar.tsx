927a23373915bbfb074a2112b96c273c0be3e99d
DylanCowell <Dylanacowell@gmail.com>
2026-02-10 00:29:39 -0500

    switch order again

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -15,8 +15,8 @@ export default function Navbar() {
   const navLinks = [
     { name: 'Home', href: '#home' },
     { name: 'About', href: '#about' },
-    { name: 'Projects', href: '#projects' },
     { name: 'Freelance', href: '#freelance' },
+    { name: 'Projects', href: '#projects' },
     { name: 'Skills', href: '#skills' },
     { name: 'Contact', href: '#contact' },
   ]
